986f6af37f87a8ce448e5b6aba12456baa0e1d85
thinhnguyen597 <thinhnguyen597@gmail.com>
2023-06-24 18:03:00 +0700

    thong-bao-trang-chu.js

--- thong-bao-trang-chu.js
+++ thong-bao-trang-chu.js
@@ -1,2 +1,2 @@
 /*! jQuery v3.2.1 | (c) JS Foundation and other contributors | jquery.org/license */
-document.write("Thông báo đổi tên từ Me Seeding thành hethonglike.vn");
+document.write("Thông báo đổi tên từ Me Seeding thành hethonglike.vn ngày 24/06/2023");
